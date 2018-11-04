66ef04df189ceae51a5a86adfd507f3fce413b07
pookie <pookie@pookies-MacBook-Pro.local>
2018-11-04 06:59:03 -0500

    finishing interface for pke_stack with next_to_top

--- c/lib/data_structures.h
+++ c/lib/data_structures.h
@@ -101,4 +101,18 @@ int dlist_remove(dlist_t *list, dlist_el_t *element, void **data);
 
 typedef pke_list_t pke_stack_t;
 
+#define pke_stack_init pke_list_init;
+
+#define pke_stack_destroy pke_list_destroy;
+
+int pke_stack_push(pke_stack_t *stack, const void *data);
+
+int pke_stack_pop(pke_stack_t *stack, void **data);
+
+#define pke_stack_top(stack) ((stack)->head == NULL ? NULL : (stack)->head->data)
+
+#define pke_stack_next_to_top(stack) ((stack)->head == NULL || (stack)->head->next == NULL ? NULL : (stack)->head->next->data)
+
+#define pke_stack_size pke_list_size;
+
 #endif //PKEALGORITHMS_DATA_STRUCTURES_H
